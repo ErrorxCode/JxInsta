e9dac9aa4de95b444830a20b62c921cb8d216b37
Rahil <65817230+ErrorxCode@users.noreply.github.com>
2026-04-10 01:11:33 +0530

    Delete JxInsta directory

--- JxInsta/src/main/java/com/errorxcode/jxinsta/RequestCallback.java
+++
@@ -1,8 +0,0 @@
-package com.errorxcode.jxinsta;
-
-import okhttp3.ResponseBody;
-
-public interface RequestCallback {
-    void onSuccess(ResponseBody response);
-    void onFailure(Exception e);
-}
